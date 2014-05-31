fa30c6e278ec74418df0704982beb765bd60c6ab
jartek <jartek.91@gmail.com>
2014-05-31 13:28:10 +0530

    more stuff

--- lib/transfermarkt.rb
+++ lib/transfermarkt.rb
@@ -5,18 +5,18 @@ module Transfermarkt
   class Team
     attr_reader :data
 
-    BASE_DOMAIN = "http://transfermarkt.com"
+    BASE_DOMAIN = 'http://transfermarkt.com'
 
     def initialize(url)
       @data = Nokogiri::HTML(get(url))
     end
 
     def national_team?
-      data.css(".profilheader tr").any? { |row| row.text.include?('Placement FIFA world ranking') }
+      data.css('.profilheader tr').any? { |row| row.text.include?('Placement FIFA world ranking') }
     end
 
     def squad_size
-      data.css(".profilheader tr").select { |row| row.text.include?('Squad size') }[0].css('td').text.strip.to_i
+      data.css('.profilheader tr').select { |row| row.text.include?('Squad size') }[0].css('td').text.strip.to_i
     end
 
     def players
@@ -27,9 +27,18 @@ module Transfermarkt
     end
 
     def manager
-      staff_container = data.css('.header_slider').select { |row| row.css('p.text').text.strip == "Staff" }[0].parent
+      staff_container = data.css('.header_slider').select { |row| row.css('p.text').text.strip == 'Staff' }[0].parent
       manager = staff_container.css('li.slider-list').select { |row| row.css('.container-zusatzinfo b:first-child').text.strip == 'Manager' }[0]
       BASE_DOMAIN + manager.css('.container-hauptinfo a')[0]['href']
+    end
+
+    def stadium
+      return if national_team?
+      BASE_DOMAIN + data.css('.profilheader a[href*=stadion]')[0]['href']
+    end
+
+    def fixtures
+      BASE_DOMAIN + data.css('.footer a[href*=spielplan]')[0]['href']
     end
 
     private
--- spec/transfermarkt_team_spec.rb
+++ spec/transfermarkt_team_spec.rb
@@ -44,29 +44,56 @@ describe Transfermarkt::Team do
     end
   end
 
-  describe "#players" do
+  describe '#players' do
     context 'England' do
       let(:players) { national_response.players }
-      it "returns entire squad" do
+      it 'returns entire squad' do
         expect(players.length).to eq(national_response.squad_size)
       end
 
-      it "should have sturridge" do
-        sturridge_present = players.any? { |player| player.include?("sturridge") }
+      it 'should have sturridge' do
+        sturridge_present = players.any? { |player| player.include?('sturridge') }
         expect(sturridge_present).to eq(true)
       end
 
-      it "shouldn't have hodgson" do
-        sturridge_present = players.any? { |player| player.include?("hodgson") }
+      it 'shouldn\'t have hodgson' do
+        sturridge_present = players.any? { |player| player.include?('hodgson') }
         expect(sturridge_present).to eq(false)
       end
     end
   end
 
-  describe "#manager" do
+  describe '#manager' do
     context 'England' do
-      it "returns hodgson" do
+      it 'returns hodgson' do
         expect(national_response.manager).to include('hodgson')
+      end
+    end
+    context 'Liverpool' do
+      it 'returns rodgers' do
+        expect(club_response.manager).to include('rodgers')
+      end
+    end
+  end
+
+  describe '#stadium' do
+    context 'England' do
+      it 'returns nil' do
+        expect(national_response.stadium).to be_nil
+      end
+    end
+
+    context 'Liverpool' do
+      it 'returns stadium link' do
+        expect(club_response.stadium).to include('stadion') #stadion = stadium in german
+      end
+    end
+  end
+
+  describe "#fixtures" do
+    context 'England' do
+      it 'returns stadium link' do
+        expect(national_response.fixtures).to include('spielplan') #spielplan = fixtures in german
       end
     end
   end
